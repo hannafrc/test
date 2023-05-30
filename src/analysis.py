69f35ec871931684650808e33acf5a7dbf100339
hannafrc <hannafrc@pm.me>
2023-05-30 09:31:56 +0200

    data head

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,19 @@
+# Imports
 import pandas as pd
 #from sklearn.model_selection import train_test_split
 import sklearn.model_selection as ms
 
+# Read data
+path = "C:/Users/splhal/git/test2"
+tData = pd.read_csv(path+"/data/train.csv")
+print(tData.head())
 #ms.train_test_split()
+
+
+# Explore
+
+# Modify
+
+# Model
+
+# Assess
